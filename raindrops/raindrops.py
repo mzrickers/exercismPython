0cfcdee9e12f32cd330ca3598f837564808103ef
Zach Rickers <mzrickers@gmail.com>
2020-07-29 19:26:38 -0600

    Third try raindrops

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -16,22 +16,40 @@
 #         return str(number)
 #     return sound
 
-rain_drop_dict = {
-    "Pling": 3,
-    "Plang": 5,
-    "Plong": 7
-}
-
-def convert(number, dict=rain_drop_dict):
-    return_value = ''
-    for key, value in dict.items():
-        if number % value == 0:
-            return_value += key
-    if return_value == '':
-        return str(number)
-    return return_value
+# Second Try
+# rain_drop_dict = {
+#     "Pling": 3,
+#     "Plang": 5,
+#     "Plong": 7
+# }
 
 
-test1 = convert(15, rain_drop_dict)
+# def convert(number, dict=rain_drop_dict):
+#     return_value = ''
+#     for key, value in dict.items():
+#         if number % value == 0:
+#             return_value += key
+#     if return_value == '':
+#         return str(number)
+#     return return_value
+
+
+# test1 = convert(15, rain_drop_dict)
+# print(test1)
+
+
+# Third time's the charm
+RULES = [
+    ["Pling", 3],
+    ["Plang", 5],
+    ["Plong", 7]
+]
+
+def convert(num, arr=RULES):
+    filtered = [rain_drop[0] for rain_drop in arr if num % rain_drop[1] == 0]
+    if filtered:
+        return "".join(filtered)
+    return str(num)
+
+test1 = convert(1)
 print(test1)
-
